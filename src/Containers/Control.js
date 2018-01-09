2a549585020479a955cad651277f3449684a14a4
Ashutosh Devkota <ashutosh.devkota@gmail.com>
2018-01-08 21:31:24 -0600

    load some data by user from firestore based on firebase authentication

--- src/Containers/Control.js
+++ src/Containers/Control.js
@@ -1,93 +1,103 @@
 import React, {Component} from 'react';
-import {firebaseLogin, firebaseLogout, isAuthenticated, firebaseAuth} from '../Services/firebase.service';
+import {firebaseLogin, firebaseLogout, isAuthenticated, firebaseAuth, getUserInfo} from '../Services/firebase.service';
 import './Control.css'
 
 class Control extends Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         authenticated: undefined,
-         email: '',
-         password:''
-      };
-      this.toggleAuth = this.toggleAuth.bind(this);
-      this.updateAuthInfo = this.updateAuthInfo.bind(this);
-      this.renderAuthenticated = this.renderAuthenticated.bind(this);
-      this.renderNotAuthenticated = this.renderNotAuthenticated.bind(this);
-      this.renderUnknownAuthentication = this.renderUnknownAuthentication.bind(this);
-   }
-   
-   componentWillMount() {
-      firebaseAuth().onAuthStateChanged((user) => {
-         this.setState((prevState, props) => {
-            return {
-               authenticated: !!user,
-               toggleAuthText: !user? 'Login': 'Logout'
-            }
-         });
-      })
-   }
-   
-   render() {
-      if (this.state.authenticated === true) {
-         return this.renderAuthenticated();
-      } else if(this.state.authenticated === false) {
-         return this.renderNotAuthenticated();
-      } else {
-         // if state.authenticated is not defined, do not rented anything
-         return this.renderUnknownAuthentication();
-      }
-   }
+	constructor(props) {
+		super(props);
+		this.state = {
+			authenticated: undefined,
+			email: '',
+			password:'',
+			userInfo: null
+		};
+		this.toggleAuth = this.toggleAuth.bind(this);
+		this.updateAuthInfo = this.updateAuthInfo.bind(this);
+		this.renderAuthenticated = this.renderAuthenticated.bind(this);
+		this.renderNotAuthenticated = this.renderNotAuthenticated.bind(this);
+		this.renderUnknownAuthentication = this.renderUnknownAuthentication.bind(this);
+	}
+	
+	componentWillMount() {
+		firebaseAuth().onAuthStateChanged(user => {
+			if(!!user) {
+				getUserInfo(user.uid)
+				.then(doc => {
+					this.setState( {
+						authenticated: true,
+						userInfo: doc.exists? doc.data(): null
+					});
+				});
+			} else {
+				this.setState({
+					authenticated: false,
+					userInfo: null
+				});
+			}
 
-   
-   renderAuthenticated() {
-      return (
-         <div className="Control">
-            <span>  hello </span>
-            <button onClick={this.toggleAuth}>Logout</button>
-         </div> 
-      );
-   }
-   
-   renderNotAuthenticated() {
-      return (
-         <div className="Control">
-            <span>  hello </span>
-            <input
-               name='email'
-               value={this.state.email}
-               onChange={(e)=> this.updateAuthInfo(e.target.name, e.target.value)} 
-               />
-            <input
-               name='password'
-               value={this.state.password}
-               onChange={(e)=> this.updateAuthInfo(e.target.name, e.target.value)} 
-               />
-            <button onClick={this.toggleAuth}>Login</button>
-         </div> 
-      );
-   }
-   
-   renderUnknownAuthentication() {
-      return (
-         <div className="Control">
-            <span>  hello </span>
-         </div> 
-      )
-   }
-   
-   toggleAuth() {
-      if(!isAuthenticated()) {
-         firebaseLogin(this.state.email, this.state.password);
-      } else {
-         firebaseLogout();
-      }
-      
-   }
-   
-   updateAuthInfo(key, val) {
-      this.setState({[key]: val});
-   }
+		})
+	}
+	
+	render() {
+		if (this.state.authenticated === true) {
+			return this.renderAuthenticated();
+		} else if(this.state.authenticated === false) {
+			return this.renderNotAuthenticated();
+		} else {
+			// if state.authenticated is not defined, do not rented anything
+			return this.renderUnknownAuthentication();
+		}
+	}
+
+	
+	renderAuthenticated() {
+		return (
+			<div className="Control">
+				<span>  hello {this.state.userInfo && this.state.userInfo.displayName} </span>
+				<button onClick={this.toggleAuth}>Logout</button>
+			</div> 
+		);
+	}
+	
+	renderNotAuthenticated() {
+		return (
+			<div className="Control">
+				<span>  hello </span>
+				<input
+					name='email'
+					value={this.state.email}
+					onChange={(e)=> this.updateAuthInfo(e.target.name, e.target.value)} 
+					/>
+				<input
+					name='password'
+					value={this.state.password}
+					onChange={(e)=> this.updateAuthInfo(e.target.name, e.target.value)} 
+					/>
+				<button onClick={this.toggleAuth}>Login</button>
+			</div> 
+		);
+	}
+	
+	renderUnknownAuthentication() {
+		return (
+			<div className="Control">
+				<span>  hello </span>
+			</div> 
+		)
+	}
+	
+	toggleAuth() {
+		if(!isAuthenticated()) {
+			firebaseLogin(this.state.email, this.state.password);
+		} else {
+			firebaseLogout();
+		}
+		
+	}
+	
+	updateAuthInfo(key, val) {
+		this.setState({[key]: val});
+	}
 }
-   
+	
 export default Control;
--- src/Services/firebase.service.js
+++ src/Services/firebase.service.js
@@ -3,20 +3,21 @@ const firebase = require("firebase/app");
 require("firebase/auth");
 require("firebase/firestore");
 
-// let db;
+let db;
 const localStorageKey = 'me.adevkota.react-invoice';
 
 export const firebaseAuth = firebase.auth;
 export const firebaseInit = () => {
    let config = {
-      apiKey: "AIzaSyAiqxhHbuMn3H0HmBR-BdDWckuCbX77o-w",
-      authDomain: "invoice-aa086.firebaseapp.com",
-      databaseURL: "https://invoice-aa086.firebaseio.com",
-      projectId: "invoice-aa086",
-      storageBucket: "invoice-aa086.appspot.com",
-      messagingSenderId: "1090928850629"
+		apiKey: "AIzaSyAiqxhHbuMn3H0HmBR-BdDWckuCbX77o-w",
+		authDomain: "invoice-aa086.firebaseapp.com",
+		databaseURL: "https://invoice-aa086.firebaseio.com",
+		projectId: "invoice-aa086",
+		storageBucket: "invoice-aa086.appspot.com",
+		messagingSenderId: "1090928850629"
    };
    firebase.initializeApp(config);
+   db = firebase.firestore();
    // firebaseAuth().onAuthStateChanged(handleAuthStateChange);
 }
 
@@ -30,19 +31,24 @@ export const firebaseInit = () => {
 
 export const firebaseLogin = (email, pw) => {
    firebaseAuth()
-      .signInWithEmailAndPassword(email, pw)
-      .catch((error) => {
-         console.log(error);
-      })
+	.signInWithEmailAndPassword(email, pw)
+	.catch((error) => {
+	   console.log(error);
+	})
 }
 export const firebaseLogout = () => {
    firebaseAuth()
-      .signOut()
-      .catch((error) => {
-         console.log(error);
-      })
+	.signOut()
+	.catch((error) => {
+	   console.log(error);
+	})
 }
 
 export const isAuthenticated = () => {
    return !!firebaseAuth().currentUser || !!localStorage.getItem(localStorageKey);
 }
+
+export const getUserInfo = (id) => {
+	return db.collection('users').doc(id).get();
+}
+	
